81545eda74f32649b8f820c47fdd0a36e89a13b2
Jorge Alcivar Hurtado Duarte <jahurtado11@utpl.edu.ec>
2020-07-10 19:14:47 -0500

    Add budget

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Budget } from './components/budget/budget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Budget
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/budget/budget.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component ({
+    selector: 'budget',
+    templateUrl: './budget.component.html'
+
+})
+export class Budget {
+    title: 'Presupuesto';
+}
